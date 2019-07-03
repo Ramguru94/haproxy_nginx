62dd5b7de0debef7236cf617a34299f54e9a4a14
Ramguru Nageswaran <ramguru.nageswaran@zoomrx.com>
2019-07-03 10:59:43 +0530

    adding scripts

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
+set -x
 install () {
   sudo apt-get clean
   sudo apt-get update
   sudo apt-get install haproxy -y
+  config
 }
 
 config () {
   sudo rm /etc/haproxy/haproxy.cfg
-  sed 
+  sudo cp /home/vagrant/haproxy.cfg /etc/haproxy/haproxy.cfg
+  sudo /etc/init.d/haproxy restart
 }
 install
--- scripts/slave.sh
+++ scripts/slave.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-sudo apt-get clean
-sudo apt-get update -y
+set -x
 
-sudo apt-get install nginx -y
-sudo service nginx start
+install() {
+  sudo apt-get clean
+  sudo apt-get update -y
+  sudo apt-get install -y nginx
+  sudo service nginx start
+  sudo rm /var/www/html/*
+  sudo cp /home/vagrant/index.html /var/www/html/index.html
+}
+install
